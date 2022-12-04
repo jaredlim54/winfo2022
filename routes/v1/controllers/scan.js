084fcc1793eb5cf6716b514394a7e43d4b81cff2
Duy Hoang Nguyen <duyhoangnguyen2000@gmail.com>
2022-12-03 18:40:14 -0800

    m

--- routes/v1/controllers/scan.js
+++ routes/v1/controllers/scan.js
@@ -14,6 +14,12 @@ const router = express.Router();
 // router.get('/', async (req, res) => {
 
 // })
+let conversion = []
+router.get('/', (req, res)=>{
+    res.send(conversion)
+})
+
+
 router.post('/', async (req, res) => {
     const { pdf } = req.files;
     pdf.mv(`./pdf/${pdf.name}`);
@@ -64,11 +70,10 @@ router.post('/', async (req, res) => {
         saveFilename: "untitled",
         savePath: "./images",
         format: "jpg",
-        width: 600,
-        height: 600
+        width: 1000,
+        height: 800
     };
-    let pageNumber = 1;
-    let isBase64 = true;
+    
     const image = fromBuffer(pdf.data, options)
     const worker = await createWorker({
         logger: m => console.log(m)
@@ -80,19 +85,16 @@ router.post('/', async (req, res) => {
     let p1 = await image(2)
     console.log(p1)
     const { data: { text } } = await worker.recognize(await readFile(p1.path));
-    console.log(text);
     await worker.terminate();
-
-    console.log(text)
+    conversion.push(text)
     // const text = `
     // The Power of Technology
 
     // Our inventions change the world, and the reinvented world changes us. Human life on Earth today looks radically different from just a century ago, thanks in good part to technologies invented in the intervening years. Once firmly earthbound, with only legs and wheels to carry us on land and ships to cross the waters, we have now taken to flight in droves, with more than eight million passengers criss-crossing continents each day in a few airborne hours. If Rich-ard Branson, found of Virgin Galactic, achieves his dream of building the world's first commercial "spaceline," ordinary people may soon become astronauts. Communication, too, has broken free from shackles of time and distance. When I left India in the mid-1950s, it took three weeks for letters to go back and forth from Kolkata, where I was born, to Scarsdale, New York, where my family first settled. Mail would not arrive reliably. Stamps would be stolen and packages not delivered. Today, an electronic message sent at night from the eastern United States brings an instant reply from a friend in Europe or Asia whose day is just beginning. Facebook connects more than a billion users worldwide with a single mouse click or two. Last but not least, we have cracked the secrets of living
     // `;
     
+    res.json({"status":"success"})
 
-    res.type("text");
-    res.send(text);
 });
 
 
